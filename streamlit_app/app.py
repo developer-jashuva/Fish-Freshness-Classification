658e25a2773334f8dc0e21bcea43f02601d9d5e2
developer-jashuva <srijashuva@gmail.com>
2025-03-30 10:37:35 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -37,7 +37,7 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image",  use_container_width=True)
 
     # Preprocess and predict
     input_tensor = preprocess_image(image)
